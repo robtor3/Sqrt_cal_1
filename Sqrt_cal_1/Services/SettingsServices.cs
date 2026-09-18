6c25f7c88a2cab97b23ec68eb6ce6902f4dd362f
Robtor <no2078233@gmail.com>
2026-09-18 20:56:41 +0500

    добавлены комментарии в SettingsServices

--- Sqrt_cal_1/Services/SettingsServices.cs
+++ Sqrt_cal_1/Services/SettingsServices.cs
@@ -3,31 +3,30 @@ using Sqrt_cal_1.Services;
 using System.Numerics;
 namespace Sqrt_cal_1.Services;
 
-public partial class SettingsServices : ObservableObject
+public partial class SettingsServices : ObservableObject // объявляем класс SettingsServices, наследуем ObservableObject чтобы класс мог уведомить подписанные на него компоненты об изменениях в своих свойств
 {
     [ObservableProperty]
-    private int precision = 2;
+    private int precision = 2; // поле с точностью по умолчанию
 
-    [ObservableProperty]
-    private bool useInvariantCulture = false;
-
+    //[ObservableProperty]
+    //private bool useInvariantCulture = false;
 
     public string Format(double value)
         => value.ToString("F" + Precision,
-            System.Globalization.CultureInfo.InvariantCulture);
+            System.Globalization.CultureInfo.InvariantCulture); // форматирование (для действительных чисел) через fixed point формата "F" + кол-во знаков после запятой, Precision, берётся из свойства, сгенерированного [ObservableProperty]
 
     public string FormatComplex(Complex c)
     {
-        if (Math.Abs(c.Imaginary) < 1e-12)
+        if (Math.Abs(c.Imaginary) < 1e-12) // если мнимая часть комплексного числа очень мала, считаем её нулём; форматируем и возвращаем только действительную часть
             return Format(c.Real);
 
-        string re = Format(c.Real);
-        string im = Format(Math.Abs(c.Imaginary));
-        string sign = c.Imaginary < 0 ? "-" : "+";
+        string re = Format(c.Real); // форматируем действ часть
+        string im = Format(Math.Abs(c.Imaginary)); // форматируем мнимую часть (по модулю)
+        string sign = c.Imaginary < 0 ? "-" : "+"; // знак перед мнимой частью компл числа
 
         if (Math.Abs(c.Real) < 1e-12)
-            return (c.Imaginary < 0 ? "-" : "") + im + "i";
+            return (c.Imaginary < 0 ? "-" : "") + im + "i"; // если действительная часть комплексного числа очень мала, считаем её нулём; форматируем и возвращаем только мнимую часть
 
-        return $"{re} {sign} {im}i";
+        return $"{re} {sign} {im}i"; // возвращаем в формате действ часть +- мнимая часть
     }
 }
